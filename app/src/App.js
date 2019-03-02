77c62fba07d60b970678aba7e7d22912268d87fe
Zooheck <davidscottflack@gmail.com>
2019-03-02 16:47:56 -0600

    Create react app and basic setup.

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/NavBar/NavBar.js';
+import Search from './components/Search/Search.js';
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ app/src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+export default function NavBar() {
+  return (
+    <div>
+      <AppBar title="PixaBay Image Finder" />
+    </div>
+  )
+}
