410161c9d7eab45f34b50819e6383225cf70999d
Zooheck <davidscottflack@gmail.com>
2019-03-03 15:38:58 -0600

    Open image modal on click

--- app/src/components/ImageResults/ImageResults.js
+++ app/src/components/ImageResults/ImageResults.js
@@ -9,6 +9,21 @@ import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 
 class ImageResults extends Component {
+    state = {
+        open: false,
+        currentImg: ''
+    }
+    handleOpen = img => {
+        this.setState({
+            open: true,
+            currentImg: img
+        })
+    }
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
     render() {
         let imageListContent;
         const { images } = this.props;
@@ -26,7 +41,7 @@ class ImageResults extends Component {
                                 </span>
                             }
                             actionIcon={
-                                <IconButton>
+                                <IconButton onClick={() => this.handleOpen(image.largeImageURL)}>
                                     <ZoomIn color="white" />
                                 </IconButton>
                             }
@@ -39,9 +54,21 @@ class ImageResults extends Component {
         } else {
             imageListContent = null;
         }
+
+        const actions = [
+            <FlatButton label="Close" primary={true} onClick={this.handleClose}/>
+        ]
         return (
             <div>
                 {imageListContent}
+                <Dialog
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                >
+                    <img src={this.state.currentImg} alt="" style={{ width: '100%'}} />
+                </Dialog>
             </div>
         );
     }
